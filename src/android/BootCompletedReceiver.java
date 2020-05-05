6c2d86a739adc32837b3b9f0ab98eed8d90a27c9
WMC <th@wmc-card.no>
2020-05-05 13:01:26 +0200

    Update BootCompletedReceiver.java

--- src/android/BootCompletedReceiver.java
+++ src/android/BootCompletedReceiver.java
@@ -4,13 +4,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.widget.Toast;
 import com.tonikorin.cordova.plugin.autostart.AppStarter;
  
 public class BootCompletedReceiver extends BroadcastReceiver {
     
     @Override
     public void onReceive(Context context, Intent intent) {
-        
+        Toast.makeText(context,"Broadcast Received in Activity called ",Toast.LENGTH_LONG).show();
         AppStarter appStarter = new AppStarter();
         appStarter.run(context, intent, PackageManager.COMPONENT_ENABLED_STATE_ENABLED, true);
     }
